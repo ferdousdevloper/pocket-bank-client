0e58dd25e44bee579aec6bbb8124ce05b1f4b293
Your Nameferdousdevloper <eng.mfhossain@gmail.com>
2024-07-17 21:23:57 +0600

    improve UI

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,4 @@
-
-import {  useState } from "react";
+import { useState } from "react";
 import { Toaster } from "react-hot-toast";
 import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
 import useAdmin from "../../Hook/useAdmin";
@@ -9,10 +8,10 @@ import useUser from "../../Hook/useUser";
 import Swal from "sweetalert2";
 import useBlockUser from "../../Hook/useBlockUser";
 import usePending from "../../Hook/usePending";
-
+import bgImage from "../../../public/bg.png"; // Import your background image here
+import logo from "../../../public/logo.png"
 const Dashboard = () => {
   const navigate = useNavigate();
-  // const [users, setUsers] = useState([]);
   const [isAdmin, isAdminLoading] = useAdmin();
   const [isAgent, isAgentLoading] = useUser();
   const [isBlock, isBlockLoading] = useBlockUser();
@@ -36,15 +35,9 @@ const Dashboard = () => {
   if (isPending === undefined)
     return <div>Error: Unable to determine Pending status</div>;
 
-  console.log(isAdmin);
-  console.log(isAgent);
-  console.log(isBlock);
-  console.log(isPending);
-
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
-
 
   const handleLogout = () => {
     localStorage.removeItem("token"); // Remove token from local storage
@@ -59,99 +52,146 @@ const Dashboard = () => {
 
   return (
     <>
-      <div className="relative">
+      <div
+        className="relative"
+        style={{
+          backgroundImage: `url(${bgImage})`, // Apply background image here
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
         {/* dashboard side bar */}
         <div
           className={` ${
             sidebarOpen ? "translate-x-0" : "-translate-x-[450px]"
-          } w-32 md:w-64 min-h-screen bg-gradient-to-r from-violet-800 to-fuchsia-800 text-white   absolute   md:px-10 py-8  transform transition-transform ease-in-out duration-500 z-50`}
+          } w-32 md:w-64 min-h-full bg-gradient-to-r  from-violet-800 to-fuchsia-800  text-white   absolute   md:px-10 py-8  transform transition-transform ease-in-out duration-500 z-50`}
         >
-          <ul className="menu p-4">
-            <li>
+          <div>
+            <img src={logo} alt="" className="" />
+          </div>
+          <ul className="menu p-4 ">
+            <li className="my-4">
               <NavLink to="/dashboard">Overview</NavLink>
             </li>
             {isAdmin ? (
               <>
-                <li>
+                <li className="my-4">
                   <Link to="/dashboard/userManagement">User Management</Link>
                 </li>
-                <li>
+                <li className="my-4">
                   <NavLink to="/dashboard/systemMonitoring">
                     System Monitoring
                   </NavLink>
                 </li>
               </>
             ) : (
-              <>{isPending ? (<>
-                <p className="text-yellow-500">Youre account is pending....</p>
-                <p className="text-yellow-500">Youre account will be active in few hours!!</p>
-              </>):(<>
-                {isBlock ? (
+              <>
+                {isPending ? (
                   <>
-                    <p className="text-red-600">Youre Blocked By Admin!!</p>
+                    <p className="text-yellow-500 my-4">
+                      Youre account is pending....
+                    </p>
+                    <p className="text-yellow-500 my-4">
+                      Youre account will be active in few hours!!
+                    </p>
                   </>
                 ) : (
                   <>
-                    <>
-                      {isAgent ? (
-                        <>
-                          <li>
-                            <NavLink to="/dashboard/transactionManagement">
-                              Transaction Management
-                            </NavLink>
-                          </li>
-                          <li>
-                            <NavLink to="/dashboard/balanceInquiry">
-                              Balance Inquiry
-                            </NavLink>
-                          </li>
-                          <li>
-                            <NavLink to="/dashboard/transactionHistory">
-                              Transactions History
-                            </NavLink>
-                          </li>
-                        </>
-                      ) : (
-                        <>
-                          <li>
-                            <NavLink to="/dashboard/sendMoney">
-                              Send Money
-                            </NavLink>
-                          </li>
-                          <li>
-                            <NavLink to="/dashboard/cashOut">Cash Out</NavLink>
-                          </li>
-                          <li>
-                            <NavLink to="/dashboard/cashIn">Cash In</NavLink>
-                          </li>
-                          <li>
-                            <NavLink to="/dashboard/balanceInquiry">
-                              Balance Inquiry
-                            </NavLink>
-                          </li>
-                          <li>
-                            <NavLink to="/dashboard/transactionHistory">
-                              Transactions History
-                            </NavLink>
-                          </li>
-                        </>
-                      )}
-                    </>
+                    {isBlock ? (
+                      <>
+                        <p className="text-red-600 my-4">
+                          Youre Blocked By Admin!!
+                        </p>
+                      </>
+                    ) : (
+                      <>
+                        {isAgent ? (
+                          <>
+                            <li className="my-4">
+                              <NavLink
+                                to="/dashboard/transactionManagement"
+                                activeClassName="text-blue-400"
+                              >
+                                Transaction Management
+                              </NavLink>
+                            </li>
+                            <li className="my-4">
+                              <NavLink
+                                to="/dashboard/balanceInquiry"
+                                activeClassName="text-blue-400"
+                              >
+                                Balance Inquiry
+                              </NavLink>
+                            </li>
+                            <li className="my-4">
+                              <NavLink
+                                to="/dashboard/transactionHistory"
+                                activeClassName="text-blue-400"
+                              >
+                                Transactions History
+                              </NavLink>
+                            </li>
+                          </>
+                        ) : (
+                          <>
+                            <li className="my-4">
+                              <NavLink
+                                to="/dashboard/sendMoney"
+                                activeClassName="text-blue-400"
+                              >
+                                Send Money
+                              </NavLink>
+                            </li>
+                            <li className="my-4">
+                              <NavLink
+                                to="/dashboard/cashOut"
+                                activeClassName="text-blue-400"
+                              >
+                                Cash Out
+                              </NavLink>
+                            </li>
+                            <li className="my-4">
+                              <NavLink
+                                to="/dashboard/cashIn"
+                                activeClassName="text-blue-400"
+                              >
+                                Cash In
+                              </NavLink>
+                            </li>
+                            <li className="my-4">
+                              <NavLink
+                                to="/dashboard/balanceInquiry"
+                                activeClassName="text-blue-400"
+                              >
+                                Balance Inquiry
+                              </NavLink>
+                            </li>
+                            <li className="my-4">
+                              <NavLink
+                                to="/dashboard/transactionHistory"
+                                activeClassName="text-blue-400"
+                              >
+                                Transactions History
+                              </NavLink>
+                            </li>
+                          </>
+                        )}
+                      </>
+                    )}
                   </>
                 )}
-              </>)}</>
-              
+              </>
             )}
             <button
               onClick={handleLogout}
-              className="mt-4 bg-red-600 text-white p-2 rounded"
+              className="mt-4 bg-red-600 w-full text-white p-2 rounded"
             >
               Logout
             </button>
           </ul>
         </div>
-        <label className="md:px-40 px-16 pt-10 btn btn-circle swap swap-rotate flex justify-end w-full text-2xl md:text-5xl items-center bg-gray-700 text-white">
-          {/* this hidden checkbox controls the state */}
+        <label className="md:px-40 px-16 pt-10 btn btn-circle swap swap-rotate flex justify-end w-full text-2xl md:text-5xl items-center  text-white">
+          {/* Hamburger menu */}
           <input type="checkbox" hidden onClick={toggleSidebar} />
           {sidebarOpen ? (
             <>
@@ -164,7 +204,7 @@ const Dashboard = () => {
           )}
         </label>
         {/* dashboard content */}
-        <div className="p-8 w-full min-h-screen bg-gray-700  text-white">
+        <div className="p-8 w-full min-h-screen text-white">
           <div className="w-3/5 mx-auto">
             <Outlet />
             <Toaster />
@@ -176,3 +216,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
